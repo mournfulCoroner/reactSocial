5da3384a8e5d70790aeb08bd091a6adf0cafba5d
mournful <linayn9@gmail.com>
2021-08-06 00:49:06 +0300

    'starter_pack'

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+function Header(){
+    return (
+        <div>
+            <iu>
+                <li>Раз</li>
+                <li>Два</li>
+                <li>Три</li>
+            </iu>
+        </div>
+    )
+}
+
+export default Header;
