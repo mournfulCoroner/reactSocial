87d432e861288e8b9b18242e2926fe83da15dcd7
mournful <linayn9@gmail.com>
2021-08-28 01:05:01 +0300

    gh-graph

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,17 +1,21 @@
+import React, { Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import Login from '../Login/Login';
+import Preloader from './common/Preloader/Preloader';
 import styles from './Content.module.css';
-import DialogsContainer from './Dialogs/DialogsContainer';
-import ProfileContainer from './Profile/ProfileContainer';
 import UsersContainer from './Users/UsersContainer';
+
+const DialogsContainer = React.lazy(() => import('./Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./Profile/ProfileContainer'))
 
 function Content(props) {
     return (
         <div className={styles.content}>
-            <Route path='/profile/:userId?' render={() => <ProfileContainer />}></Route>
-            <Route path='/messages' render={() => <DialogsContainer />}></Route>
+            <Suspense fallback={<Preloader />}>
+                <Route path='/profile/:userId?' render={() => <ProfileContainer />}></Route>
+                <Route path='/messages' render={() => <DialogsContainer />}></Route></Suspense>
             <Route path='/users' render={() => <UsersContainer />}></Route>
-            <Route path='/login' render={() => <Login />}></Route>        
+            <Route path='/login' render={() => <Login />}></Route>
         </div>
     );
 }
