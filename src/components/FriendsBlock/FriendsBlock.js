040947b3d1c08b946a33d1f19e24756947ad3198
mournful <linayn9@gmail.com>
2021-08-28 02:57:36 +0300

    button to hide friends

--- src/components/FriendsBlock/FriendsBlock.js
+++ src/components/FriendsBlock/FriendsBlock.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import FriendPile from './FriendPile/FriendPile';
 import styles from './FriendsBlock.module.css';
 
@@ -5,19 +6,19 @@ import styles from './FriendsBlock.module.css';
 const FriendsBlock = (props) => {
     let friends = props.friendBar.friends.map((friend) => <FriendPile key={friend.id} id={friend.id} friend_name={friend.friend_name} />);
     
-    // let open_friends = () => {
-    //     stateFr = !stateFr;
-    // }
-    
-    let stateFr = false;
+    let [isOpenFriends, setIsOpenFriends] = useState(false);
+    let open_friends = (e) => {
+        setIsOpenFriends(!isOpenFriends);
+        !isOpenFriends ? e.currentTarget.style.left = '116px' : e.currentTarget.style.left = '0px'
+    }
     
     return (
         <div className={styles.fblock}>
-            {/* <div className={styles.fblock__close}>
-                <button className={styles.fblock__button} onClick={open_friends}>rk</button>
-            </div> */}
+            <div className={styles.fblock__close}>
+                <button className={styles.fblock__button} onClick={ open_friends }>{'>'}</button>
+            </div>
             <div className={
-                stateFr ? `${styles.fblock__open} ${styles.active}` : `${styles.fblock__open}`}>
+                isOpenFriends ? `${styles.fblock__open} ${styles.active}` : `${styles.fblock__open}`}>
                 <p className={styles.ftitle}>Palms</p>
                 <div className={styles.fblock__friends}>
                     {friends}
